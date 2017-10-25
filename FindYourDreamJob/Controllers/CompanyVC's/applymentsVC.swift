815ed5b3280da530bcbd0de2e059d4f7ba7ccf39
onur hüseyin çantay <onurhuseyincantay@onurs-MacBook-Pro.local>
2017-10-26 00:14:38 +0300

    Added some different constraint for each device ProfileVC Design Began

---
+++ FindYourDreamJob/Controllers/CompanyVC's/applymentsVC.swift
@@ -0,0 +1,35 @@
+//
+//  applymentsVC.swift
+//  FindYourDreamJob
+//
+//  Created by onur hüseyin çantay on 24.10.2017.
+//  Copyright © 2017 onur hüseyin çantay. All rights reserved.
+//
+
+import UIKit
+
+class applymentsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FindYourDreamJob/Model/CompanyUser.swift
@@ -0,0 +1,9 @@
+//
+//  CompanyUser.swift
+//  FindYourDreamJob
+//
+//  Created by onur hüseyin çantay on 24.10.2017.
+//  Copyright © 2017 onur hüseyin çantay. All rights reserved.
+//
+
+import Foundation
