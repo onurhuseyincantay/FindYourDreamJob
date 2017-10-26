c764d5d587941ffeede0abbe2d83f43f2070a02d
onur hüseyin çantay <onurhuseyincantay@onurs-MacBook-Pro.local>
2017-10-26 20:21:33 +0300

    Fixed some landscape problems finished the profileVC UI and started with JobPostingVC UI

---
+++ FindYourDreamJob/Views/ProfileVCTextFields.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileVCTextFields.swift
+//  FindYourDreamJob
+//
+//  Created by onur hüseyin çantay on 26.10.2017.
+//  Copyright © 2017 onur hüseyin çantay. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVCTextFields: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
